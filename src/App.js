c7a6bebe195e5387dc17c1cff4341d457d4ceb84
Tafara Shamu <talph@outlook.com>
2020-06-22 16:59:16 +0200

    updated menu-items to navigate withRouter HOC

--- src/App.js
+++ src/App.js
@@ -1,12 +1,34 @@
 import React from 'react';
 import './App.css';
-
+import { Route, Switch } from 'react-router-dom';
 import HomePage from './pages/homepage/homepage.component';
+
+const HatsPage = (props) => {
+  console.log(props);
+  return (
+  <div>
+    <h1>HATS</h1>
+  </div>
+  )
+}
+
+const TopicDetail = (props) => {
+  console.log(props);
+  return (
+  <div>
+    <h1>Topic detail {props.match.params.topicId}</h1>
+  </div>
+  )
+}
 
 function App() {
   return (
     <div>
-      <HomePage />
+<Switch>
+      <Route exact path="/" component={HomePage}/>
+      <Route exact  path="/hats" component={HatsPage}/>
+      <Route exact  path="/topics/:topicId" component={TopicDetail} />
+</Switch>
     </div>
   );
 }
